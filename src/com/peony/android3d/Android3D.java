f00707348d78d33225556412d744f76dc8d6bc33
wang ying <wyylling@gmail.com>
2012-11-25 11:02:59 +0800

    configure eclipse cdt

---
+++ src/com/peony/android3d/Android3D.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2012
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *  Created on: Nov 25, 2012
+ *  Author:     wyylling
+ *  Email:      wyylling@gmail.com
+ */
+
+package com.peony.android3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.util.Log;
+
+
+public class Android3D extends Activity implements SurfaceHolder.Callback {
+
+    private static String TAG = "android3d";
+    
+    private Surface mSurface;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i(TAG, "onCreate()");
+
+        nativeOnCreate();
+        
+        setContentView(R.layout.main);
+        SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surfaceview);
+        surfaceView.getHolder().addCallback(this);
+        surfaceView.setOnClickListener(new OnClickListener() {
+                public void onClick(View view) {
+                    Toast toast = Toast.makeText(Android3D.this,
+                                                 "This demo combines Java UI and native EGL + OpenGL renderer",
+                                                 Toast.LENGTH_LONG);
+                    toast.show();
+                }});
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+        nativeOnResume();
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+        nativeOnPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onDestroy();
+        Log.i(TAG, "onStop()");
+        nativeOnStop();
+        finish();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+    	mSurface = holder.getSurface();
+        nativeSetSurface(mSurface);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        nativeSetSurface(null);
+    }
+
+
+    public static native void nativeOnCreate();
+    public static native void nativeOnResume();
+    public static native void nativeOnPause();
+    public static native void nativeOnStop();
+    public static native void nativeSetSurface(Surface surface);
+
+    static {
+        System.loadLibrary("android3d_jni");
+    }
+
+}
