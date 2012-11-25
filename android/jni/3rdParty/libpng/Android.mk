5df10c91989522c3a1b867a845fa23e7bf3a685d
wang ying <wyylling@gmail.com>
2012-11-25 12:09:24 +0800

    add libpng support

---
+++ android/jni/3rdParty/libpng/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libpng
+
+LOCAL_LDLIBS := -lz
+LOCAL_SRC_FILES := \
+	$(subst $(LOCAL_PATH)/,, \
+	$(wildcard $(LOCAL_PATH)/../../../src/3rdParty/libpng-android/*.c))
+
+$(info $(LOCAL_SRC_FILES)) 
+
+include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_STATIC_LIBRARY)
